fe56c29d33228b81706a46defe10d6ccac377065
Max Halford <maxhalford25@gmail.com>
2018-09-29 00:41:08 +0200

    Pour Geoff

--- submissions/blend.py
+++ submissions/blend.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 
 files = {
-    'max.csv': 1.15,
+    'max.csv': 1.3,
     'raph.csv': 1,
-    'adil.csv': 1.05,
+    'adil.csv': 1,
 }
 
 subs = {file: pd.read_csv(file) for file in files}
@@ -14,7 +14,7 @@ print(pd.DataFrame({file: sub['target'] for file, sub in subs.items()}).corr())
 blend = subs[list(subs.keys())[0]].copy()
 blend['target'] = 0
 for file, sub in subs.items():
-    blend['target'] += files[file] * sub['target']
-blend['target'] /= sum(files.values())
+    blend['target'] *= (files[file] * sub['target'])
+blend['target'] **= 1 / sum(files.values())
 
 blend.to_csv('blend.csv', index=False)
